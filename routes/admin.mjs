d2222764ff1ec49c15fe246407f4c67d9975e8c9
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-12-09 19:33:16 -0800

    completed admin middleware

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import * as bookController from '../controllers/bookController.mjs';
 import pool from '../config/db.mjs';
+import { ensureAdmin } from '../middleware/ensureAuthenticated.mjs';
 
 const router = Router();
 
-// Explore Page Route
-router.get('/admin', async (req, res) =>
+// Apply ensureAdmin middleware to all admin routes
+router.get('/admin', ensureAdmin, async (req, res) =>
 {
- 
+    res.render('admin');
+});
+
+router.get('/admin/addbook', ensureAdmin, async (req, res) =>
+{
     res.render('admin');
 });
 
