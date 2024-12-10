05abedb2f83e403a1ed893537d49ca9705cec077
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-12-09 19:34:59 -0800

    removed unnecessary imports

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-import * as bookController from '../controllers/bookController.mjs';
-import pool from '../config/db.mjs';
 import { ensureAdmin } from '../middleware/ensureAuthenticated.mjs';
 
 const router = Router();
