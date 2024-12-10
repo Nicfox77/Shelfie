1384b51203316a7b0b3706153e0ce4e4573b5cff
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-12-09 21:15:26 -0800

    worked on add book functionality

--- routes/admin.mjs
+++ routes/admin.mjs
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ensureAdmin } from '../middleware/ensureAuthenticated.mjs';
+import pool from '../config/db.mjs';
 
 const router = Router();
 
@@ -11,12 +12,46 @@ router.get('/admin', ensureAdmin, async (req, res) =>
 
 router.get('/admin/addbook', ensureAdmin, async (req, res) =>
 {
-    res.send('admin add book');
+    res.render('addbook');
+});
+
+router.post('/admin/addbook', ensureAdmin, async (req, res) =>
+{
+    let isbn = req.body.isbn;
+    let title = req.body.title;
+    let author = req.body.author;
+    let description = req.body.description;
+    let publisher = req.body.publisher;
+    let publishDate = req.body.publishDate;
+    let pageCount = req.body.pageCount;
+    let rating = req.body.rating;
+    let genre = req.body.genre;
+    let imgUrl = req.body.imgUrl;
+
+    let params = [isbn, title, author, description, publisher, publishDate, pageCount, rating, genre, imgUrl];
+    let conn;
+
+    try
+    {
+        conn = await pool.getConnection();
+        let sql = `INSERT INTO Books (isbn, title, author, description, publisher, published_date, page_count, rating, genre, image) 
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        await conn.query(sql,params);
+
+        res.redirect('/admin');
+    } catch (error)
+    {
+        console.error('Error adding book:', error);
+        res.status(500).send('Error adding book to database');
+    } finally
+    {
+        if (conn) conn.release();
+    }
 });
 
 router.get('/admin/searchuser', ensureAdmin, async (req, res) =>
 {
-    res.send('admin add user');
+    res.render('admin');
 });
 
 export default router;
