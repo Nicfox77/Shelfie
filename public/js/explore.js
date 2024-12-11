aef853ae94df1c0bc8076de9f2d7cdd396e064d4
Josh Hong <161401987+HongJosh@users.noreply.github.com>
2024-12-11 14:03:54 -0800

    Fixed typo and added comments

--- public/js/explore.js
+++ public/js/explore.js
@@ -10,17 +10,17 @@ function sortBooks() {
     let bookContainer = document.querySelector("#bookResults");
     let books = Array.from(bookContainer.querySelectorAll(".exploreBook")); // Get array of books from book container
 
-    // Sory books based on sortValue
+    // Sort books based on sortValue
     books.sort((a, b) => {
         if (sortValue === "rating") {
-            return b.dataset.rating - a.dataset.rating; 
+            return b.dataset.rating - a.dataset.rating; // Sort by rating
         } else if (sortValue === "newest") {
-            return new Date(b.dataset.published_date) - new Date(a.dataset.published_date);
+            return new Date(b.dataset.published_date) - new Date(a.dataset.published_date); // Sort by published date
         } else if (sortValue === "alphabetical") {
-            return a.dataset.title.localeCompare(b.dataset.title);
+            return a.dataset.title.localeCompare(b.dataset.title); // Sort by title (alphabetical)
         }
     });
 
     bookContainer.innerHTML = "";
-    books.forEach(book => bookContainer.appendChild(book));
+    books.forEach(book => bookContainer.appendChild(book)); // Add sorted list of books back to book container
 }
