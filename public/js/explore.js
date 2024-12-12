0df033ae2653517006545bffe57f931229ee0ce4
Josh Hong <161401987+HongJosh@users.noreply.github.com>
2024-12-11 16:04:39 -0800

    Added form validation for explore search

--- public/js/explore.js
+++ public/js/explore.js
@@ -1,10 +1,25 @@
 // Event Listeners
+document.querySelector("#exploreForm").addEventListener("submit", function(event) {
+    validateForm(event);
+});
+
 const sortElement = document.querySelector("#sort");
 if (sortElement) {
     sortElement.addEventListener("change", sortBooks);
 }
 
 // Functions
+function validateForm(e) {
+    document.querySelector("#searchError").innerHTML = "";
+    let search = document.querySelector(".exploreSearch").value; // get search value
+    // Display error message if there is no search or is an empty string
+    if (!search || search == "") {
+        document.querySelector("#searchError").innerHTML = "Please enter a search";
+        document.querySelector("#searchError").style.color = "Red";
+        e.preventDefault();
+    }
+}
+
 function sortBooks() {
     let sortValue = document.querySelector("#sort").value;
     let bookContainer = document.querySelector("#bookResults");
